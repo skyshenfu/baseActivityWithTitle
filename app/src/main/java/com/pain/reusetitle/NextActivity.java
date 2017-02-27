3c0d6376fccb7b1efa8e21fe574486afb53fd398
painworkpc <zhangty@all-help.com>
2017-02-27 17:15:48 +0800

    clear waring

--- app/src/main/java/com/pain/reusetitle/NextActivity.java
+++ app/src/main/java/com/pain/reusetitle/NextActivity.java
@@ -6,7 +6,7 @@ public class NextActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setActivityType(this.TITLEBARONLYBACK,"Next页面");
+        setActivityType(TITLEBARONLYBACK,"Next页面");
         setContentView(R.layout.activity_next);
     }
 }
